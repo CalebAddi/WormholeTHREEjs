42e01a7bb627af712b97c18dff4a319c27788460
Caleb Addison <caleb.addi.77@gmail.com>
2025-06-07 01:17:48 -0500

    updates to camera functionality

--- src/main.js
+++ src/main.js
@@ -14,13 +14,32 @@ camera.position.set(0, 0, 40);
 
 const composer = lighting(scene, camera, renderer);
 
-// const hemiLight = new THREE.HemisphereLight(0xffffff, 0x444444, 5);
-// scene.add(hemiLight);
+// Create tube geometry from spline
+const tubeGeo = new THREE.TubeGeometry(spline, 222, 1.5, 20, true);
 
-function animate()
+// Create edge geometry from spline
+const edgeGeo = new THREE.EdgesGeometry(tubeGeo, 0.2);
+const lineMat = new THREE.LineBasicMaterial({ color: 0x00ffff });
+const lineEdges = new THREE.LineSegments(edgeGeo, lineMat);
+scene.add(lineEdges);
+
+function updateCamera(t)
+{
+    const time = t * 0.075;
+    const loopTime = 10 * 1000; // 10 seconds
+    const t2 = (time % loopTime) / loopTime; // Normalize time [0, 1]
+    const pos = tubeGeo.parameters.path.getPointAt(t2);
+    const lookAt = tubeGeo.parameters.path.getPointAt((t2 + 0.03) % 1); // Look ahead slightly
+    camera.position.copy(pos);
+    camera.lookAt(lookAt);
+}
+
+
+function animate(t = 0)
 {
     requestAnimationFrame(animate);
 
+    updateCamera(t);
     composer.render();
 }
 animate();
